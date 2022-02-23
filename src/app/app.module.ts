1bfe9a8490199c5b24d038ecfa82fed490517e79
ThaisSantiago <thaisrpm10@gmail.com>
2022-02-23 18:02:14 -0300

    Componente início sem consumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +29,12 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { EntrarComponent } from '../entrar/entrar.component';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
+import { EntrarComponent } from '../entrar/entrar.component'
 
 
 @Injectable({
@@ -26,4 +27,13 @@ export class AuthService {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
   
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token != ""){
+      ok = true
+    }
+
+    return ok
+  }
 }
